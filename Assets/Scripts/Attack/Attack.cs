877b57ffb22d9b4cb2eb4223b931f0aac7749551
Lucas <lucasmaximinotorres@gmail.com>
2024-07-28 20:05:24 -0300

    weakness system

--- Assets/Scripts/Attack/Attack.cs
+++ Assets/Scripts/Attack/Attack.cs
@@ -1,12 +1,26 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [Serializable]
-public struct Attack {
+public struct Attack
+{
     [SerializeField]
     public float damage;
     [SerializeField]
     public AttackType type;
     [SerializeField]
     public ElemetType element;
+
+    public static Attack operator *(Attack attack, int attackMultiplier)
+    {
+        attack.damage *= attackMultiplier;
+        return attack;
+    }
+
+    public static Attack operator *(Attack attack, float attackMultiplier)
+    {
+        attack.damage *= attackMultiplier;
+        return attack;
+    }
 }
--- Assets/Scripts/Characters/Mobs/MobTest.cs
+++ Assets/Scripts/Characters/Mobs/MobTest.cs
@@ -2,21 +2,77 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Health))]
+[RequireComponent(typeof(StatusEffect))]
+[RequireComponent(typeof(Attacker))]
+[RequireComponent(typeof(Defender))]
 public class MobTest : MonoBehaviour
 {
-    private int test = 0;
-    void turnPrep(){
-        Debug.Log("test turn prep: "  + test++);
-        GameManager.gm.turnManager.addCurrentTurnAction(new() {
-            action = turnAction,
-            validation = true
-        });
+
+    private float defenseDebufRatio = 0.75f;
+
+    private Health health;
+    private StatusEffect statusEffect;
+
+    private Attacker attacker;
+    private Defender defender;
+
+    private int debufDuration = 2;
+    private int debufCurrentDuration = 0;
+
+    private Dictionary<ElemetType, ElemetType> weakness = new(){
+        { ElemetType.WATER,ElemetType.EARTH},
+        { ElemetType.EARTH,ElemetType.AIR },
+        { ElemetType.FIRE,ElemetType.WATER },
+        { ElemetType.AIR,ElemetType.FIRE },
+
+    };
+    void turnPrep()
+    {
+        Debug.Log("Mob turn prep, debuf duration: " + debufCurrentDuration);
+        Debug.Log("Current defense: " + defender.defenseRatio);
+
+        if (debufCurrentDuration >= debufDuration)
+        {
+            debufCurrentDuration = 0;
+            defender.defenseRatio /= defenseDebufRatio;
+        }
     }
 
-    void turnAction(){
+    void turnAction()
+    {
         Debug.Log("Mob Turn Action");
     }
-    void Start(){
+    void Start()
+    {
         GameManager.gm.turnPrep += turnPrep;
+        health = GetComponent<Health>();
+        statusEffect = GetComponent<StatusEffect>();
+        attacker = GetComponent<Attacker>();
+        defender = GetComponent<Defender>();
+        statusEffect.onEffects += onEffect;
+
+
+        defender.onAttack += (Attack attack) =>
+        {
+            statusEffect.applyEffect(attack.element);
+            health.takeDamage(attack.damage);
+            Debug.Log(health.health);
+        };
+    }
+
+    void onEffect(ElemetType currentType, ElemetType newType)
+    {
+        Debug.Log("Mob on effect");
+        if (this.weakness[currentType] == newType)
+        {
+            if (debufCurrentDuration == 0)
+            {
+                this.defender.defenseRatio *= defenseDebufRatio;
+                this.debufCurrentDuration = 1;
+            }
+
+        }
+
     }
 }
